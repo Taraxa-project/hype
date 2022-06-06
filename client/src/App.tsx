91d5b21ef7cefa38e08c6d03c67d1441794be8cf
Elod23 <vargaelod23@gmail.com>
2022-06-06 17:46:06 +0300

    Add background hover to app on modal open

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,34 @@
-import React from "react";
-import "./App.css";
-import Header from "./components/header/Header";
-import { useMediaQuery } from "react-responsive";
-import useMetamask from "./hooks/useMetamask";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { MetaMaskProvider } from "metamask-react";
-import Home from "./pages/Home/Home";
+import './App.css';
+import Header from './components/header/Header';
+import { useMediaQuery } from 'react-responsive';
+import useMetamask from './hooks/useMetamask';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { MetaMaskProvider } from 'metamask-react';
+import Home from './pages/Home/Home';
+import BackgroundHover from './components/background/HoverBackground.styled';
+import { useModal } from './hooks/useModal';
 
 function Root() {
   const isMobile = useMediaQuery({ query: `(max-width: 760px)` });
   const { status, connect, account } = useMetamask();
-
+  const {isOpen} = useModal();
   return (
     <div className="App">
-        <Header
-          variant={isMobile ? "mobile" : "desktop"}
-          status={
-            status === "initializing" || status === "connecting"
-              ? "notConnected"
-              : status
-          }
-          account={account}
-          onConnect={connect}
-          children={null}
-        />
-        <div className="appContainer">
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Home} />
-            </Switch>
-          </Router>
-        </div>
+      <Header
+        variant={isMobile ? 'mobile' : 'desktop'}
+        status={status === 'initializing' || status === 'connecting' ? 'notConnected' : status}
+        account={account}
+        onConnect={connect}
+      >
+      </Header>
+      <div className="appContainer">
+        <BackgroundHover show={isOpen}/>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
