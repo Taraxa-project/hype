8640c8d2c24f4add877d30fc667d7d0f69594c07
Elod23 <vargaelod23@gmail.com>
2022-07-01 16:35:43 +0300

    Fix warning

--- services/contracts/contracts/ERC20Base.sol
+++ services/contracts/contracts/ERC20Base.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // Example class - a mock class using delivering from ERC20
 contract ERC20Base is ERC20 {
-    constructor(uint256 initialBalance) ERC20("Basic", "BSC") public {
+    constructor(uint256 initialBalance) ERC20("Basic", "BSC") {
         _mint(msg.sender, initialBalance);
     }
 }
