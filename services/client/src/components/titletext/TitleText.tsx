5bd3418dadf8bff37f7315a28eb788e6e375cc22
Facundo Rebord <farebord@gmail.com>
2022-07-15 14:28:40 -0300

    rename titletext folder

---
+++ services/client/src/components/titletext/TitleText.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default styled.h3`
+  font-weight: 700;
+  font-size: 1.5rem;
+  line-height: 1.625rem;
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.colors.black};
+`;
