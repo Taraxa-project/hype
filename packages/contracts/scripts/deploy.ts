b78a779eead5a169fe7cc54583a1741ee0142e5b
Elod23 <vargaelod23@gmail.com>
2022-07-19 15:37:24 +0200

    Add deploy script and commands

--- packages/contracts/scripts/deploy.ts
+++ packages/contracts/scripts/deploy.ts
@@ -3,7 +3,10 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -13,9 +16,29 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  // We get the contract to deploy
-  const HypePool = await ethers.getContractFactory("HypePool");
-  const hypePool = await HypePool.deploy();
+  const FEE_DATA = {
+    maxFeePerGas: ethers.utils.parseUnits("10000", "gwei"),
+    maxPriorityFeePerGas: ethers.utils.parseUnits("500", "gwei"),
+    gasPrice: ethers.utils.parseUnits("1000", "gwei"),
+  };
+
+  // Wrap the provider so we can override fee data.
+  const provider = new ethers.providers.JsonRpcProvider(process.env.TARA_TESTNET_URL);
+  provider.getFeeData = async () => FEE_DATA;
+
+  // Create the signer for the mnemonic, connected to the provider with hardcoded fee data
+  const signer = new ethers.Wallet(process.env.MAINNET_PRIV_KEY || "").connect(provider);
+  console.log("signer address: ", signer.address);
+
+  // Get the contract factory connected to signer so it uses hardcoded fee data and
+  // should deploy using the signer and the hardcoded fees.
+  const DynamicEscrow = await ethers.getContractFactory("DynamicEscrow", signer);
+  const dynamicEscrow = await upgrades.deployProxy(DynamicEscrow, ["0xc6a808A6EC3103548f0b38d32DCb6a705B734c89"]);
+
+  await dynamicEscrow.deployed();
+  console.log("DynamicEscrow deployed to:", dynamicEscrow.address);
+  const HypePool = await ethers.getContractFactory("HypePool", signer);
+  const hypePool = await upgrades.deployProxy(HypePool, [dynamicEscrow.address]);
 
   await hypePool.deployed();
 
