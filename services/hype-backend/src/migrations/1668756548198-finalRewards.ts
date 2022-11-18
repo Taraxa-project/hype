be38f2aa57bc4794f52b05f6188a66b36f80aba8
Elod23 <vargaelod23@gmail.com>
2022-11-18 16:28:24 +0200

    commits new migrations

---
+++ services/hype-backend/src/migrations/1668756548198-finalRewards.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class src1668756548198 implements MigrationInterface {
+  name = 'src1668756548198';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "hype_reward" ("id" SERIAL NOT NULL, "amount" character varying NOT NULL, "rewardee" character varying NOT NULL, "tokenAddress" character varying NOT NULL, "claimed" boolean NOT NULL DEFAULT false, "poolId" integer, CONSTRAINT "PK_6a1502d10e0da9d2cbef3017ebf" PRIMARY KEY ("id"))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "hype_reward" ADD CONSTRAINT "FK_573687ddf0c5c68e39a6c9a38bb" FOREIGN KEY ("poolId") REFERENCES "hype_pool"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "hype_reward" DROP CONSTRAINT "FK_573687ddf0c5c68e39a6c9a38bb"`,
+    );
+    await queryRunner.query(`DROP TABLE "hype_reward"`);
+  }
+}
