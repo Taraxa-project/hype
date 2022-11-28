a94ea6854fbc2f80c1721ee560b1c0fa8f3936fd
Elod23 <vargaelod23@gmail.com>
2022-11-28 14:14:20 +0200

    remove unnecessary definitions

--- services/hype-backend/config/ethereum.ts
+++ services/hype-backend/config/ethereum.ts
@@ -3,7 +3,6 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('ethereum', () => ({
   escrowContractAddress: process.env.ESCROW_CONTRACT_ADDRESS,
   hypeContractAddress: process.env.HYPE_CONTRACT_ADDRESS,
-  claimContractAddress: process.env.CLAIM_CONTRACT_ADDRESS,
   privateSigningKey: process.env.PRIVATE_SIGNING_KEY,
   provider: process.env.PROVIDER,
 }));
