6ceb3dd6fa420c3ec80deb126e4c9c594d3e9b1b
Florin Georgescu <flo_georgescu@hotmail.com>
2022-12-06 15:33:27 +0200

    fix(hype-backend): Fixed dependencies for reward module

--- services/hype-backend/src/modules/reward/reward.module.ts
+++ services/hype-backend/src/modules/reward/reward.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth';
 import { BlockchainModule } from '../blockchain';
 import { RewardController } from './reward.controller';
 import { HypeReward } from './reward.entity';
 import { RewardService } from './reward.service';
 
 @Module({
-  imports: [BlockchainModule, TypeOrmModule.forFeature([HypeReward])],
+  imports: [
+    BlockchainModule,
+    TypeOrmModule.forFeature([HypeReward]),
+    AuthModule,
+  ],
   controllers: [RewardController],
   providers: [RewardService],
   exports: [RewardService],
