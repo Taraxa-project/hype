35184808bfb3d6b3f596c06ef6f6e590b37f474e
Elod23 <vargaelod23@gmail.com>
2022-12-08 19:04:51 +0200

    implement symbol and pool info fetching

--- services/client/src/utils/tokens.ts
+++ services/client/src/utils/tokens.ts
@@ -2,12 +2,14 @@ import { ethers } from 'ethers';
 import ABIs from '../abi';
 
 export function getERC20TContract(address: string) {
-  return new ethers.Contract(address, ABIs.contracts.HypePool.abi);
+  return new ethers.Contract(address, ABIs.contracts.HypeToken.abi);
 }
 
 export async function getERC20TokenName(address: string) {
-  const contract = getERC20TContract(address);
-  if (contract) {
-    return await contract.symbol();
-  } else return 'TARA';
+  if (address) {
+    const contract = getERC20TContract(address);
+    if (contract && contract.provider) {
+      return await contract.symbol();
+    } else return 'TARA';
+  }
 }
