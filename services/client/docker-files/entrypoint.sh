5f620ae8ddbc27c0c7662cd6f53b1cd0e5e3a2b1
Leonard Mocanu <leonard.mocanu@ymail.com>
2023-02-17 16:32:08 +0200

    chore(ui): removes ipfs env vars

--- services/client/docker-files/entrypoint.sh
+++ services/client/docker-files/entrypoint.sh
@@ -6,10 +6,7 @@ export GRAPHQL_HOST=${GRAPHQL_HOST:="https://indexer.qa.hype.taraxa.io/subgraphs
 export TELEGRAM_BOT_NAME=${TELEGRAM_BOT_NAME:="HypeAppBot"}
 export HYPE_ADDRESS=${HYPE_ADDRESS:="0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"}
 export ESCROW_ADDRESS=${ESCROW_ADDRESS:="0x5FbDB2315678afecb367f032d93F642f64180aa3"}
-export IPFS_HOST=${IPFS_HOST:="ipfs.infura.io"}
-export IPFS_PORT=${IPFS_PORT:="5001"}
-export IPFS_PROTOCOL=${IPFS_PROTOCOL:="https"}
-export IPFS_USE_AUTH=${IPFS_USE_AUTH:=false}
+export IPFS_BASE_URL=${IPFS_BASE_URL:="https://gateway.qa.hype.taraxa.io/ipfs/"}
 
 
 export STATIC_FOLDER="/usr/share/nginx/html"
@@ -18,6 +15,7 @@ export STATIC_FOLDER="/usr/share/nginx/html"
 API_HOST=${API_HOST%/}
 WALLET_HOST=${WALLET_HOST%/}
 GRAPHQL_HOST=${GRAPHQL_HOST%/}
+IPFS_BASE_URL=${IPFS_BASE_URL%/}
 
 
 echo "Replace from env vars..."
@@ -28,9 +26,6 @@ find $STATIC_FOLDER -type f -exec sed -i "s,REACT_APP_GRAPHQL_HOST_STRING_REPLAC
 find $STATIC_FOLDER -type f -exec sed -i "s,REACT_APP_TELEGRAM_BOT_NAME_STRING_REPLACE,$TELEGRAM_BOT_NAME,g" {} \;
 find $STATIC_FOLDER -type f -exec sed -i "s,REACT_APP_HYPE_ADDRESS_STRING_REPLACE,$HYPE_ADDRESS,g" {} \;
 find $STATIC_FOLDER -type f -exec sed -i "s,REACT_APP_ESCROW_ADDRESS_STRING_REPLACE,$ESCROW_ADDRESS,g" {} \;
-find $STATIC_FOLDER -type f -exec sed -i "s,REACT_APP_IPFS_HOST_STRING_REPLACE,$IPFS_HOST,g" {} \;
-find $STATIC_FOLDER -type f -exec sed -i "s,REACT_APP_IPFS_PORT_STRING_REPLACE,$IPFS_PORT,g" {} \;
-find $STATIC_FOLDER -type f -exec sed -i "s,REACT_APP_IPFS_PROTOCOL_STRING_REPLACE,$IPFS_PROTOCOL,g" {} \;
-find $STATIC_FOLDER -type f -exec sed -i "s,REACT_APP_IPFS_USE_AUTH_STRING_REPLACE,$IPFS_USE_AUTH,g" {} \;
+find $STATIC_FOLDER -type f -exec sed -i "s,REACT_APP_IPFS_BASE_URL_STRING_REPLACE,$IPFS_BASE_URL,g" {} \;
 
 exec "$@"
