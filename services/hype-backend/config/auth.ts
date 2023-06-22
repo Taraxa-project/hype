55739a59c0b3d138a98c3d590e3b5a81e3f1b737
Florin Georgescu <flo_georgescu@hotmail.com>
2023-06-22 13:07:05 +0300

    Changed the default jwt expiry time to 7 days

--- services/hype-backend/config/auth.ts
+++ services/hype-backend/config/auth.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('auth', () => ({
   walletEndpoint: process.env.WALLET_HOST || 'http://localhost:3006',
   secret: process.env.AUTH_SECRET || 'secret',
-  tokenExpiry: process.env.JWT_EXPIRY_TIME || '30 days',
+  tokenExpiry: process.env.JWT_EXPIRY_TIME || '7 days',
   gsSecret: process.env.GS_SECRET,
   subGraphEndpoint:
     process.env.SUBGRAPH_HOST ||
