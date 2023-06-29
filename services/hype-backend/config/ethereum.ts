4cfbb8bd30e22cc89d12f176e05dcc440c09cf27
Florin Georgescu <flo_georgescu@hotmail.com>
2023-06-29 20:54:01 +0300

    Fix issue with useToken when not custom token

--- services/hype-backend/config/ethereum.ts
+++ services/hype-backend/config/ethereum.ts
@@ -1,8 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('ethereum', () => ({
-  escrowContractAddress: process.env.ESCROW_CONTRACT_ADDRESS,
-  hypeContractAddress: process.env.HYPE_CONTRACT_ADDRESS,
   privateSigningKey: process.env.PRIVATE_SIGNING_KEY,
   provider: process.env.PROVIDER,
 }));
