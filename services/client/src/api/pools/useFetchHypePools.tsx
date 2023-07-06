d453d2c670237aac6f3bcb1941c2f7e60ac9a83b
Florin Georgescu <flo_georgescu@hotmail.com>
2023-07-06 17:24:21 +0300

    Toggle active/inactive pools

--- services/client/src/api/pools/useFetchHypePools.tsx
+++ services/client/src/api/pools/useFetchHypePools.tsx
@@ -10,35 +10,53 @@ interface FetchHypesFilter {
   orderBy: 'endDate' | 'startDate' | 'creator' | 'remainingFunds' | 'cap';
   orderDirection: 'desc' | 'asc';
   text?: string;
+  endDate_gt?: number;
+  endDate_lt?: number;
 }
 
-const computeFilters = (page: number, search?: string): FetchHypesFilter => {
+const computeFilters = (page: number, active: boolean, search?: string): FetchHypesFilter => {
   const first = hypePoolsPerPage;
   const skip = (page - 1) * hypePoolsPerPage;
   const orderBy = 'endDate';
   const orderDirection = 'desc';
 
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const defaultTimestamp = 0; // For endDate_lt
+  const futureTimestamp = Math.floor(Date.now() / 1000) + 10 * 365 * 24 * 60 * 60;
+
   const filters: FetchHypesFilter = {
     first,
     skip,
     orderBy,
-    orderDirection
+    orderDirection,
   };
 
   if (search) {
     filters.text = `'${search.trim()}'`;
   }
 
+  if (active) {
+    filters.endDate_gt = currentTimestamp;
+    filters.endDate_lt = futureTimestamp;
+  } else {
+    filters.endDate_gt = defaultTimestamp;
+    filters.endDate_lt = currentTimestamp;
+  }
+
   return filters;
 };
 
-export const useFetchHypePools = (page: number, searchString: string) => {
+export const useFetchHypePools = (page: number, active: boolean, searchString: string) => {
   const query = searchString ? HYPEPOOL_QUERIES.poolsSearchQuery : HYPEPOOL_QUERIES.poolsQuery;
-  const computedFilters: FetchHypesFilter = computeFilters(page, searchString);
-  const [result] = useQuery({
+  const computedFilters: FetchHypesFilter = computeFilters(page, active, searchString);
+  const [result, reexecuteQuery] = useQuery({
     query: query,
     variables: computedFilters,
   });
+
+  // useEffect(() => {
+  //   reexecuteQuery({ requestPolicy: 'network-only' });
+  // }, [reexecuteQuery, active]);
 
   const { data, fetching, error } = result;
 
