005c6264ca95d2b1e475183c6ecf8d22c4d52142
Florin Georgescu <flo_georgescu@hotmail.com>
2023-07-25 15:35:52 +0300

    Added handle block for subgraph

--- packages/subgraph/src/hype-pool.ts
+++ packages/subgraph/src/hype-pool.ts
@@ -5,8 +5,10 @@ import {
   PoolRewardsCreated,
   PoolUriSet,
 } from '../generated/HypePool/HypePool';
-import { HypePool, HypeUri } from '../generated/schema';
+import { HypeIds, HypePool, HypeUri } from '../generated/schema';
 import { BigInt, ethereum, ipfs, json } from '@graphprotocol/graph-ts';
+
+const DEFAULT_HYPE_IDS = '1';
 
 export function handlePoolActivated(event: PoolActivated): void {
   const id = event.params.poolId;
@@ -19,6 +21,10 @@ export function handlePoolActivated(event: PoolActivated): void {
     hypepool.status = 'STARTED';
     hypepool.startDate = event.params.startDate;
     hypepool.endDate = event.params.endDate;
+    let hypeids = HypeIds.load(DEFAULT_HYPE_IDS);
+    if (hypeids && hypeids.ids) {
+      hypeids.ids.push(id);
+    }
   }
   hypepool.save();
 }
@@ -107,8 +113,46 @@ export function handlePoolUriSet(event: PoolUriSet): void {
 
 export function handleBlock(block: ethereum.Block): void {
   let blockNumber = block.number.toI32();
+  if (blockNumber === 1) {
+    let hypeIds = new HypeIds(DEFAULT_HYPE_IDS);
+    hypeIds.ids = [];
+    hypeIds.save();
+  }
 
-  if (blockNumber % 100 === 0) {
-    // Do something every 100 blocks.
+  if (blockNumber % 1000 === 0) {
+    // Do something every 1000 blocks.
+    let hypeIds = HypeIds.load(DEFAULT_HYPE_IDS);
+    if (hypeIds && hypeIds.ids && hypeIds.ids.length > 0) {
+      for (let i = 0; i < hypeIds.ids.length; i++) {
+        let hypepool = HypePool.load(hypeIds.ids[i]);
+        if (hypepool) {
+          if (hypepool.remainingFunds.equals(BigInt.zero())) {
+            hypepool.status = 'EXPIRED';
+            // Remove pool from array of pool ids
+            hypeIds.ids.splice(i, 1);
+            hypeIds.save();
+          }
+          if (
+            hypepool
+              .endDate!.plus(BigInt.fromI32(604800))
+              .gt(BigInt.fromI32(block.timestamp.toI32())) &&
+            hypepool.endDate!.lt(BigInt.fromI32(block.timestamp.toI32()))
+          ) {
+            // If the current time is after the pool's end date, but before the end of the grace period (end date + 1 week), set status to GRACE_PERIOD
+            hypepool.status = 'GRACE_PERIOD';
+          }
+          if (
+            hypepool
+              .endDate!.plus(BigInt.fromI32(604800))!
+              .lt(BigInt.fromI32(block.timestamp.toI32()))
+          ) {
+            // Remove pool from array of pool ids
+            hypeIds.ids.splice(i, 1);
+            hypeIds.save();
+          }
+          hypepool.save();
+        }
+      }
+    }
   }
 }
